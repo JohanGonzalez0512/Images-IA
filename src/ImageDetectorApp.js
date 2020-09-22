49399898344bd58cd9e66486428e7a0c7bee0fe1
Johan Gonzalez <Jjopi20101@gmail.com>
2020-09-21 23:14:55 -0500

    first commit

---
+++ src/ImageDetectorApp.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import InputFile from './components/InputFile';
+import TableResults from './components/TableResults';
+import { analyzeImage } from './helpers/analyzeImage';
+import { usePrediction } from './hooks/usePrediction';
+
+const ImageDetectorApp = () => {
+
+    const [dataImage, setDataImage] = useState(null);
+    const { data, loading } = usePrediction(dataImage)
+
+
+
+    return (
+        <div className="m-5">
+
+            <h1>Image classification</h1>
+            <div className="row mt-3">
+                <div className="col-5">
+
+                    <img className="rounded " id="img" src={(!dataImage) ? "./assets/images/img-icon.png" : dataImage} alt="Imagen" style={{ maxWidth: 450 }} />
+
+                </div>
+
+                <div className="col-5">
+                    <InputFile setDataImage={setDataImage} />
+                    {/* <button className="btn btn-primary">analyzeImage</button> */}
+
+                    {
+                        (data.length === 0 && !loading) && <div className="alert alert-primary"> Seleccione una imagen, por favor</div>
+                    }
+                    {
+                        (loading)
+                            ? <div class="text-center">
+                                <div class="spinner-border m-5 text-primary" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                            : <TableResults info={data} />
+
+
+                    }
+                </div>
+
+
+
+            </div>
+        </div>
+    );
+}
+
+export default ImageDetectorApp;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ImageDetectorApp from './ImageDetectorApp';
+
+
+ReactDOM.render(
+  
+    <ImageDetectorApp />,
+ 
+  document.getElementById('root')
+);
+
+
